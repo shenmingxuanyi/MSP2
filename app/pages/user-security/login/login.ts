97a0e54085e7a0918a8ccd1e5460bdeb3f064bdc
shenmingxuanyi <sm.xy@live.com>
2016-08-15 09:35:24 +0800

    change

--- app/pages/user-security/login/login.ts
+++ app/pages/user-security/login/login.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {NavController, Events} from 'ionic-angular';
+import {NavController, Events, LoadingController, ToastController} from 'ionic-angular';
 import {EVENTS} from "../../../config/event.config";
 import {I18NService} from "../../../providers/i18n-service/i18n-service";
 import {UserService} from "../../../providers/user-service/user-service"
@@ -18,16 +18,28 @@ export class LoginPage {
     username: string;
     password: string;
 
-    constructor(private nav: NavController, private events: Events, public i18NService: I18NService, private userService: UserService) {
+    constructor(private nav: NavController, private events: Events, public i18NService: I18NService, private userService: UserService, private loadingController: LoadingController, private toastController: ToastController) {
 
     }
 
     login() {
+        let loader = this.loadingController.create({
+            content: "正在登陆...",
+            duration: 3000
+        });
+        loader.present();
         this.userService.signIn({username: this.username, password: this.password})
             .then((userInfo)=> {
+                loader.dismiss();
                 console.log(userInfo);
             })
             .catch(error=> {
+                loader.dismiss().then(()=> {
+                    this.toastController.create({
+                        message: error.message,
+                        duration: 3000
+                    }).present();
+                });
                 console.log(error);
             });
     }
