a1bb664fa1b55814d44afbcecdb7a3803593d685
shenmingxuanyi <sm.xy@live.com>
2016-08-16 16:01:13 +0800

    change

---
+++ app/model/business/IDCard.ts
@@ -0,0 +1,26 @@
+/**
+ * Created by Z.JM on 2016/8/16.
+ */
+export class IDCard {
+
+    name: string;
+
+    cardNum: string;
+
+    gender: string;
+
+    nation: string;
+
+    birthday: string;
+
+    registInstitution: string;
+
+    validStartDate: string;
+
+    validEndDate: string;
+
+    photoPath: string;
+
+    address: string;
+
+}
--- app/pages/bank-card/create-bank-card/create-bank-card.ts
+++ app/pages/bank-card/create-bank-card/create-bank-card.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {I18NService} from "../../../providers/i18n-service/i18n-service";
+import {IDCard} from "../../../model/business/IDCard";
+import {IDCardService} from "../../../providers/id-card-service/id-card-service";
 
 /*
  Generated class for the CreateBankCardPage page.
@@ -10,13 +12,24 @@ import {I18NService} from "../../../providers/i18n-service/i18n-service";
  */
 @Component({
     templateUrl: 'build/pages/bank-card/create-bank-card/create-bank-card.html',
+    providers: [IDCardService]
 })
 export class CreateBankCardPage {
 
+    originalIDCard: IDCard;
+    verificationIDCard: IDCard;
+
     step: number;
 
-    constructor(private navCtrl: NavController, public i18NService: I18NService) {
+    constructor(private navCtrl: NavController, public i18NService: I18NService, public idCardService: IDCardService) {
 
+    }
+
+
+    readIDCard() {
+        this.idCardService.read().then(idCardInfo=> {
+            this.originalIDCard = idCardInfo;
+        });
     }
 
     ionViewWillEnter() {
@@ -27,7 +40,6 @@ export class CreateBankCardPage {
         if (this.step > 0) {
             this.step--;
         }
-
     }
 
     nextStep() {
